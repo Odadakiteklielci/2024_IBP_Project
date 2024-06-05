70e1f0c42f56a374b4becebfc9a4fe8d582df9e0
Odadakiteklielci <ahmetharun.kaya@hotmail.com>
2024-06-05 22:58:29 +0300

    webproje

---
+++ proje1/baglanti.php
@@ -0,0 +1,23 @@
+<?php
+
+$vt_sunucu="localhost";
+$vt_kullanici="root";
+$vt_sifre="";
+$vt_adi="tatilce";
+
+
+$baglan=mysqli_connect($vt_sunucu,$vt_kullanici,$vt_sifre,$vt_adi);
+
+if(!$baglan){
+    die("Veritabanı bağlantı işlemi başarısız".mysqli_connect_error);
+}
+
+
+
+
+
+
+
+
+
+?>
---
+++ proje1/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="OWL/owl.carousel.min.css">
+        <link rel="stylesheet" href="OWL/owl.theme.default.min.css">
+        <title>Anasayfa | Tatilce</title>
+
+    </head>
+    <body>
+        <section id="menu">
+            <div id="logo">
+                <img class="ikon" height="100px" width="100px" src="img\Logo.png" alt="">
+                <span class="logo-text">TATİLCE</span>
+            </div>
+            <nav>
+                <a href="#menu"><img class="ikon" height="13px" width="13px" src="img\Home button.png" alt=""> Anasayfa</a>
+                <a href="#Hakkimizda"><img class="ikon" height="13px" width="13px" src="img\İnfo Button.png" alt="">Hakkımızda</a>
+                <a href="#turlar"><img class="ikon" height="13px" width="13px" src="img\Tours Button.png" alt="">Turlar</a>
+                <a href="#ekiph3"><img class="ikon" height="13px" width="13px" src="img\About Button.png" alt="">Ekip</a>
+                <a href="#iletisimh"><img class="ikon" height="13px" width="13px" src="img\Contact Button.png" alt="">İletişim</a>
+            </nav>
+        </section>
+        
+        <section id="anasayfa">
+            <div id="black"></div>
+            <div id="icerik">
+                <h2>Hayallerinizdeki Tatil için Tatilce!</h2>
+                <hr width="375px" width="2px" align="left">
+                <p>Tatilce, size özlediğiniz tatil deneyimini sunar.
+                   Muhteşem doğa manzaralarıyla çevrili, keyif dolu <br> aktivitelerle bezenmiş ve konforlu konaklama seçenekleri sunan yerlerimizde gerçek bir tatilin keyfini <br> çıkarın. Sizi bekleyen unutulmaz bir kaçış için şimdi Tatilce'ye katılın!
+                </p>
+            </div>
+        </section>
+        <section id="Hakkimizda">
+            <h3>Hakkımızda</h3>
+            <div class="container">
+                <div id="sol">
+                    <h5 id="h5sol">Tatilce: <br> Hayallerinizdeki Tatili Gerçeğe Dönüştürüyoruz!</h5>
+                </div>
+                <div id="sag">
+                    <span>T</span>
+                    <p id="psag">atilce olarak, seyahat tutkunlarına 
+                        hayallerindeki tatili yaşama fırsatı
+                        sunuyoruz. Amacımız, müşterilerimize
+                        unutulmaz ve keyifli bir tatil 
+                        deneyimi yaşatmak için titizlikle
+                        çalışmaktır. Yenilikçi yaklaşımımız
+                        ve sektördeki deneyimimizle,
+                        her müşterinin ihtiyaçlarını
+                        karşılayacak özel tatil
+                        paketleri sunuyoruz. Profesyonel
+                        ekibimiz ve misafirperver hizmet
+                        anlayışımızla, Tatilce olarak 
+                        tatilinizin her anını özel kılmak
+                        için buradayız. 
+                        Bizimle hayallerinizdeki tatili
+                        keşfedin ve unutulmaz anılar 
+                        biriktirin!
+                    </p>
+                </div>
+                <img src="img\about.webp" alt="" class="img-fluid mt100">
+                    <p id="pson"> Size özel turlarla dünyanın dört bir yanındaki
+                        eşsiz yerleri keşfedin. Erken rezervasyon fırsatlarımız 
+                        ile bütçenize uygun tatilinizin tadını çıkarın.</p>
+            </div>
+        </section>
+        <section id="turlar">
+            <div class="container"></div>
+            <h3>Turlar</h3>
+            <div class="owl-carousel owl-theme">
+                <div class="card item" data-merge="1.5">
+                    <img src="img\turkiye-turlari.jpg" alt="" class="img-fluid ">
+                    <h5 class="cardh">Türkiye Turu</h5>
+                    <p class="cardp">Türkiye, tarih ve doğanın muhteşem bir buluşmasıdır. Antik çağlardan günümüze kadar uzanan zengin tarihiyle sarılı, çeşitli kültürleri barındıran, nefes kesici manzaralarıyla sizi karşılamaya hazır. Tatilce'nin Türkiye turu, bu büyülü ülkeyi keşfetmenin mükemmel bir yoludur.</p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="img\italyada-mutlaka-gorulmes-738_2-41-f.jpg" alt="" class="img-fluid">
+                    <h5 class="cardh">İtalya Turu</h5>
+                    <p class="cardp">İtalya, tarihi ve lezzetleriyle ünlü bir destinasyondur. Antik Roma'nın izlerini sürmek, Rönesans'ın sanat eserlerini görmek ve Venedik'in kanallarında gezinmek için İtalya turumuza katılın! İtalya'yı keşfetmek için hazır mısınız?</p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="img\yunanaq.jpeg" alt="" class="img-fluid">
+                    <h5 class="cardh">Yunanistan Turu</h5>
+                    <p class="cardp">
+                        Yunanistan, tarihin ve doğanın büyüleyici dansıdır. Antik çağlardan günümüze kadar uzanan derin tarihiyle çevrili, çeşitli kültürlere ev sahipliği yapar ve nefes kesen manzaralarıyla sizi büyüler. Tatilce'nin Yunanistan turu, bu benzersiz ülkeyi keşfetmek için mükemmel bir fırsattır.</p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="img\isp.jpeg" alt="" class="img-fluid">
+                    <h5 class="cardh">İspanya Turu</h5>
+                    <p class="cardp">
+                        İspanya'nın muhteşem güzellikleri ve zengin kültürüyle dolu bir keşif yolculuğuna hazır mısınız? Barcelona'nın modern mimarisi, Madrid'in canlı atmosferi ve Endülüs'ün büyüleyici manzaraları gibi birçok harika yer keşfetmek için sabırsızlanıyoruz!</p>
+                </div>  
+                <div class="card item" data-merge="1.5">
+                    <img src="img\ingilte.jpg" alt="" class="img-fluid">
+                    <h5 class="cardh">İngiltere Turu</h5>
+                    <p class="cardp">
+                        İngiltere'nin tarihi hazineleri ve göz alıcı manzaralarıyla dolu bir maceraya ne dersiniz? Londra'nın görkemli Big Ben'i, Edinburgh'un mistik kaleleri ve Lake District'in eşsiz doğası gibi unutulmaz noktaları keşfetmek için sizi bekliyoruz!</p>
+                </div>  
+            </div>
+        </section>
+
+        <section id="Ekip">
+            <div class="container">
+                <h3 id="ekiph3">Ekip</h3>
+                <div class="sutun-sol-sag">
+                    <img src="img\yksbskyrd.jpg" alt="" class="img-fluid oval">
+                    <h4 class="ekipisim">Onur Bulut</h4>
+                    <p class="ekipp">Tatilce Yönetim Kurulu Başkan Yardımcısı</p>
+                    <div class="ekipicon">
+                        <a href="https://www.instagram.com/bulutt.onur?igsh=Nzl2YnZvYWhxdHh3"><i class="fa-brands fa-instagram social" style="color: white;"></i></a>
+                        <a href="mailto:onurbulut2002@hotmail.com"><i class="fa-solid fa-envelope social" style="color: white"></i></a>
+                        </div>
+                </div>
+                <div class="sutun">
+                    <img src="img\ykbsk.jpg" alt="" class="img-fluid oval">
+                    <h4 class="ekipisim">Ahmet Harun Kaya</h4>
+                    <p class="ekipp">Tatilce Yönetim Kurulu Başkanı</p>
+                    <div class="ekipicon">
+                        <a href="https://www.instagram.com/ahmetharun.kaya?utm_source=qr&igsh=cXBxeXd1M2U3ZHNv"><i class="fa-brands fa-instagram social" style="color: white;"></i></a>
+                        <a href="mailto:ahmetharun.kaya@hotmail.com"><i class="fa-solid fa-envelope social" style="color: white"></i></a>
+                        <a href="https://twitter.com/HarunKayahmet"><i class="fa-brands fa-square-x-twitter social"  style="color: white"></i></a>
+                    </div>
+                </div>
+                <div class="sutun-sol-sag">
+                    <img src="img\yksbskyrd2.jpg" alt="" class="img-fluid oval">
+                    <h4 class="ekipisim">Hikmet Çağlayan</h4>
+                    <p class="ekipp">Tatilce Yönetim Kurulu Başkan Yardımcısı</p>
+                    <div class="ekipicon">
+                        </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="iletisim">
+            <div class="container">
+                <h3 id="iletisimh">İletişim</h3>
+                <form action="index.php" method="post">
+                <div id="seffaf">
+                    <div id="formgrup">
+                        <div id="solform">
+                        <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                        <input type="text" name="tel" placeholder="Telefon Numarası" required class="form-control">
+                    </div>
+                        <div id="sagform">
+                            <input type="email" name="mail" placeholder="E-mail Adresi" required class="form-control">
+                            <input type="text" name="konu" placeholder="Konu Başlığı" required class="form-control">           
+                        </div>   
+                        <textarea name="mesaj" id="mesaj" placeholder="Mesaj Giriniz..." required class="form-control"></textarea>
+                        <input type="submit" value="Gönder">
+                    </div>
+                    <div id="adres">
+                        <h4 id="adresbaslik">Adres :</h4>
+                        <p id="adresp"> Atatürk Mahallesi 
+                        <br> <br>Akyıldız Caddesi
+                        <br> <br>Manifesto Sokak No:12
+                        <br> <br>0212 111 11 11
+                        <br> <br>tatilceiletisim@tatilce.com</p>
+                    </div>
+                </div>
+                </form>
+                <footer id="copyright">
+                    <div>2024 | Tüm Hakları Saklıdır.</div>
+                    <div id="socialfooter">
+                        <a href=""><i class="fa-brands fa-instagram social" style="color: white;"></i></a>
+                        <a href="mailto:tatilceiletisim@tatilce.com"><i class="fa-solid fa-envelope social" style="color: white"></i></a>
+                        <a href=""><i class="fa-brands fa-square-x-twitter social"  style="color: white"></i></a>
+                   
+                    </div>
+                    <a href="#menu" id="up"><i class="fa-regular fa-circle-up"></i></a>
+                </footer>
+            </div>
+        </section>
+
+        <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+        <script src="OWL/owl.carousel.min.js"></script>
+        <script src="OWL/script.js"></script>
+    </body>
+</html>
+
+
+
+
+<?php
+    include("baglanti.php");
+    
+    
+    if(isset($_POST["isim"], $_POST["tel"], $_POST["mail"],
+    $_POST["konu"], $_POST["mesaj"])){
+        $adsoyad=$_POST["isim"];
+        $telefon=$_POST["tel"];
+        $email=$_POST["mail"];
+        $konu=$_POST["konu"];
+        $mesaj=$_POST["mesaj"];
+
+        $ekle="INSERT INTO iletisim( adsoyad, telefon, email,
+         konubaslik, mesaj) VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+
+
+        if($baglan->query($ekle) === TRUE)
+        {
+
+        echo "<script>alert('Mesajınız Başarı ile Gönderilmiştir.')</script>";
+        }
+ }
+
+
+?>
